cf6c09d0d2af9b0e14dd2e83613aa3270d2c1106
AJ <anish.sj@gmail.com>
2020-11-12 02:40:01 -0500

    changed to unattended

--- gpt/scripts/gpt-data-gen-cleanup.sh
+++ gpt/scripts/gpt-data-gen-cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --env-file access-token.env -v /home/ec2-user/gpt/config:/config -v /home/ec2-user/gpt/results/:/results -it registry.gitlab.com/gitlab-org/quality/performance/gpt-data-generator --environment /config/gpt-test.json --no-vertical --clean-up
+docker run --env-file access-token.env -v /home/ec2-user/gpt/config:/config -v /home/ec2-user/gpt/results/:/results -it registry.gitlab.com/gitlab-org/quality/performance/gpt-data-generator --environment /config/gpt-test.json --no-vertical --clean-up --unattended
