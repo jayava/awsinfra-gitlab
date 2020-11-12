16d05d3e41dca42a14ade5ecd5c774a0430b3e30
AJ <anish.sj@gmail.com>
2020-11-12 02:51:12 -0500

    docker pull

---
+++ gpt/scripts/gpt-download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker pull registry.gitlab.com/gitlab-org/quality/performance/gpt-data-generator
+docker pull registry.gitlab.com/gitlab-org/quality/performance/gitlab-performance-tool
