d0ad4628fb58ad0f8fecc6fe73a45f4796ac98c1
anishjayavant <anish.sj@gmail.com>
2020-11-14 03:24:03 -0500

    version

--- network/vpc.tf
+++ network/vpc.tf
@@ -2,5 +2,7 @@ resource "aws_vpc" "gitlab-vpc" {
   cidr_block = var.vpc_cidr_range
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags = local.tags
+  tags = {
+    Name = "gitlab-vpc"
+  }
 }
