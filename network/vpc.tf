b35e75010435e6d54c09ae9ea3d746622e30f021
AJ <anish.sj@gmail.com>
2020-11-18 23:08:41 -0500

    delete vpc

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "gitlab-vpc" {
-  cidr_block = var.vpc_cidr_range
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = {
-    Name = "gitlab-vpc"
-  }
-}
+//resource "aws_vpc" "gitlab-vpc" {
+//  cidr_block = var.vpc_cidr_range
+//  enable_dns_hostnames = true
+//  enable_dns_support = true
+//  tags = {
+//    Name = "gitlab-vpc"
+//  }
+//}
