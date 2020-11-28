063cf0da341eba9ad154a4fe0ba1337a5c66384e
anishjayavant <anish.sj@gmail.com>
2020-11-28 10:35:44 -0500

    Added yaml back

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "TERRAFORM_CLOUD_WORKSPACE_NAME" {
+  description = "Name of the Terraform workspace env name"
+}
+
 variable "env" {
   description = "The name of the environment being created"
 }
