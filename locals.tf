6ac19729d936bf044e04d63322cb2e9d1a168764
anishjayavant <anish.sj@gmail.com>
2020-11-28 10:57:11 -0500

    Reading values from yaml

--- locals.tf
+++ locals.tf
@@ -4,4 +4,25 @@ locals {
   common_tags = {
     Environment = format("%s-%s", local.env.name, local.env.aws.region)
   }
+
+  subnets = local.env.network.subnets
+
+  gitlab_subnets = {
+    subnets.public.pa.name = {
+        az = subnets.public.pa.az
+        cidr = subnets.public.pa.cidr
+    }
+    subnets.public.pb.name = {
+      az = subnets.public.pb.az
+      cidr = subnets.public.pb.cidr
+    }
+    subnets.private.pa.name = {
+      az = subnets.private.pa.az
+      cidr = subnets.private.pa.cidr
+    }
+    subnets.private.pb.name = {
+      az = subnets.private.pb.az
+      cidr = subnets.private.pb.cidr
+    }
+  }
 }
--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "network" {
   source         = "./network"
   vpc_cidr_range = local.env.network.vpc-cidr
   common_tags = local.common_tags
-  gitlab_subnet_descriptors = var.gitlab_subnet_descriptors
+  gitlab_subnet_descriptors = local.gitlab_subnets
 }
