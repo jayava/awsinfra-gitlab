ba7a6bb612578c797bdd4f96f3d634d064c5ae79
anishjayavant <anish.sj@gmail.com>
2020-11-28 11:05:38 -0500

    Reading values from yaml

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
 variable "TERRAFORM_CLOUD_WORKSPACE_NAME" {
   description = "Name of the Terraform workspace env name"
 }
-
-variable "gitlab_subnet_descriptors" {
-  description = "gitlab subnet descriptors"
-}
