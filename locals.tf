f9acd6c53a6d56cf89eecd17c024eea10c044bbe
anishjayavant <anish.sj@gmail.com>
2020-11-28 10:34:15 -0500

    Added yaml back

--- locals.tf
+++ locals.tf
@@ -2,4 +2,6 @@ locals {
   common_tags = {
     Environment = format("%s-%s", var.env, var.region)
   }
+
+  env = merge(yamldecode(file("env/${var.TERRAFORM_CLOUD_WORKSPACE_NAME}.yaml")))
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "gitlab_vpc_id" {
   value = module.network.gitlab_vpc_id
 }
+
+output "env" {
+  value = local.env
+}
