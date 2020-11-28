50cfeb154be7506f0b666a128c98d6fd388c52c1
anishjayavant <anish.sj@gmail.com>
2020-11-28 01:13:35 -0500

    added subnets

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,3 +1,18 @@
 output "gitlab_vpc_id" {
   value = aws_vpc.gitlab-vpc.id
 }
+
+output "gitlab-public-subnet-a" {
+  value = aws_subnet.gitlab-public-subnet-a.id
+}
+
+output "gitlab-public-subnet-b" {
+  value = aws_subnet.gitlab-public-subnet-b.id
+}
+output "gitlab-private-subnet-a" {
+  value = aws_subnet.gitlab-private-subnet-a.id
+}
+
+output "gitlab-private-subnet-b" {
+  value = aws_subnet.gitlab-private-subnet-b.id
+}
