0efcc6ad21ad3d6d642e26f7eaef3e729770b020
anishjayavant <anish.sj@gmail.com>
2020-11-28 13:34:36 -0500

    Adding GitLab subnets to root module outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "gitlab_vpc_id" {
   value = module.network.gitlab_vpc_id
 }
 
+output "gitlab-subnets" {
+  value = module.network.gitlab-subnets
+}
+
 output "aws_region" {
   value = local.env.aws.region
 }
