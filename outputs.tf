c2d50432c81ea27cd66be1f7688dcdfe2b19d1f3
anishjayavant <anish.sj@gmail.com>
2020-11-29 01:26:48 -0500

    NAT gateway

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,12 @@ output "gitlab_vpc_id" {
   value = module.network.gitlab_vpc_id
 }
 
-output "gitlab-subnets" {
-  value = module.network.gitlab-subnets
+output "gitlab-public-subnets" {
+  value = module.network.gitlab-public-subnets
+}
+
+output "gitlab-private-subnets" {
+  value = module.network.gitlab-private-subnets
 }
 
 output "aws_region" {
