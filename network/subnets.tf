d19acbaaf020307604f76cc9afc8b3f9be21124e
anishjayavant <anish.sj@gmail.com>
2020-11-29 15:23:52 -0500

    NAT gateways and route tables

--- network/subnets.tf
+++ network/subnets.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "gitlab-public-subnets" {
 resource "aws_subnet" "gitlab-private-subnets" {
   count = length(var.availability_zones)
   vpc_id = aws_vpc.gitlab-vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr_range, 8, count.index * 2)
+  cidr_block = cidrsubnet(var.vpc_cidr_range, 8, (count.index + 1) * 2)
   availability_zone = element(var.availability_zones, count.index)
   tags = merge(var.common_tags,
   {
